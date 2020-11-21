d3c1ae64e7b3d073d0d5669b8fbb75f6a591a5e6
Casey Calkins <c.angelcalkins@outlook.com>
2020-11-21 11:33:09 -0600

    Adding dashboard cards

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import styles from './App.module.css';
-import { NavBar, Dashboard } from './components';
+import { NavBar, Dashboard, Cards } from './components';
 
 
 class App extends React.Component {
   render() {
     return (
-      <div className={styles.container}>
-        <NavBar />
-        <Dashboard />
+      <div>
+        <div className={styles.navContainer}>
+          <NavBar />
+        </div>
+        <div className={styles.dashContainer}>
+          <Cards />
+        </div>
       </div>
     )
   }
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 // Exporting components to use in App.js
 export { default as NavBar } from './NavBar/NavBar.jsx';
 export { default as Dashboard } from './Dashboard/Dashboard.jsx';
+export { default as Cards } from './Cards/Cards.jsx';
