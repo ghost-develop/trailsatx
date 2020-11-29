a23d66ff39c64bc2d76e1fb3d759ee595ae5605f
Casey Calkins <a.caseycalkins@outlook.com>
2020-11-29 15:32:19 -0600

    Starting weather widget API calls. TODO - grab the users current location then display current weather

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,10 @@
 import React from 'react';
 
 const axios = require('axios');
+
+const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        console.log(position);
+    });
+}
+getCurrentLocation();
